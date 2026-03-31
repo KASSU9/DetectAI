d44f7bab5d1a879ab8ad2d632cfcf1bf35b72232
Kasper Strang <kasper.strang@aalto.fi>
2026-03-31 21:10:10 +0300

    Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import Question from "./components/Question";
+
+function App() {
+    return (
+        <div>
+            <h1>Detect AI</h1>
+            <Question />
+        </div>
+    );
+}
+
+export default App;
---
+++ client/src/components/Question.tsx
@@ -0,0 +1,5 @@
+function Question() {
+    return <p>Question component</p>;
+}
+
+export default Question;
