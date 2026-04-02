20dcdb47e0af562c5ee9688edbdc7edc4f0e6867
Kasper Strang <kasper.strang@aalto.fi>
2026-04-02 23:27:54 +0300

    Initial guide done

--- client/src/pages/GuidePage.tsx
+++ client/src/pages/GuidePage.tsx
@@ -1,9 +1,275 @@
 function GuidePage() {
 	return (
 		<div className="card">
-			<div className="card-body">
+			<div className="card-header">
 				<h1>Guide</h1>
-				<p>This is a simple guide page for the DetectAI application.</p>
+			</div>
+			<div className="card-body">
+				<div>
+					<h2>Text content</h2>
+				</div>
+				<div>
+					<p>
+						It is important to look for many signs of AI-generated
+						content [1]. Spotting just one sign is never enough to
+						be fully convinced [1]. Here are some of the signs to
+						look for:
+					</p>
+				</div>
+				<div className="accordion" id="accordionPanelsStayOpenExample">
+					<div className="accordion-item">
+						<h2 className="accordion-header">
+							<button
+								className="accordion-button"
+								type="button"
+								data-bs-toggle="collapse"
+								data-bs-target="#panelsStayOpen-collapseOne"
+								aria-expanded="true"
+								aria-controls="panelsStayOpen-collapseOne"
+							>
+								Em dash (—)
+							</button>
+						</h2>
+						<div
+							id="panelsStayOpen-collapseOne"
+							className="accordion-collapse collapse show"
+						>
+							<div className="accordion-body">
+								The em dash (—) not to be confused with the
+								hyphen (-) is a common sign of AI-generated
+								content [1]. Em dash is also used in human
+								writing, but mainly in more formal texts. If you
+								see an em dash in a text that is supposed to be
+								informal, it might be a sign of AI-generated
+								content.
+							</div>
+						</div>
+					</div>
+					<div className="accordion-item">
+						<h2 className="accordion-header">
+							<button
+								className="accordion-button collapsed"
+								type="button"
+								data-bs-toggle="collapse"
+								data-bs-target="#panelsStayOpen-collapseTwo"
+								aria-expanded="false"
+								aria-controls="panelsStayOpen-collapseTwo"
+							>
+								Starting with a negative sentence
+							</button>
+						</h2>
+						<div
+							id="panelsStayOpen-collapseTwo"
+							className="accordion-collapse collapse"
+						>
+							<div className="accordion-body">
+								Starting with a negative sentence is another
+								sign of AI-generated content [1].
+								<br></br>
+								<br></br>
+								Examples: <br></br>
+								"Looking for signs of AI generated contents{" "}
+								<strong>isn't only about</strong> looking for
+								one sign, <strong>it's also about</strong>{" "}
+								looking for patterns and repeating signs."
+								<br></br>
+								LLMs are <strong>not just</strong> toys, they
+								help us to solve many real-world problems.
+							</div>
+						</div>
+					</div>
+					<div className="accordion-item">
+						<h2 className="accordion-header">
+							<button
+								className="accordion-button collapsed"
+								type="button"
+								data-bs-toggle="collapse"
+								data-bs-target="#panelsStayOpen-collapseThree"
+								aria-expanded="false"
+								aria-controls="panelsStayOpen-collapseThree"
+							>
+								Using lists of three items
+							</button>
+						</h2>
+						<div
+							id="panelsStayOpen-collapseThree"
+							className="accordion-collapse collapse"
+						>
+							<div className="accordion-body">
+								Using three items in a list is a common sign of
+								AI-generated content [1]. Similarly, LLMs tend
+								to prefer using an uneven number of items in a
+								list [1]. This behaviour can be found, for
+								example, in lists of adjectives and lists of
+								examples.
+								<br></br>
+								Sometimes this behaviour can also be found in
+								tandem with using emojis as line breaks [1].
+								<br></br>
+								This sign is a minor sign of AI-generated
+								content, since it's often found in human writing
+								as well [1].
+								<br></br>
+								<br></br>
+								Example: <br></br>
+								"LLMs can help you by:
+								<br></br>
+								💬 Powering scalable customer support with
+								consistent responses
+								<br></br>
+								🌎 Providing multilingual translation and
+								localization aid
+								<br></br>
+								💸 Reducing operational costs by augmenting
+								human workflows "
+							</div>
+						</div>
+					</div>
+					<div className="accordion-item">
+						<h2 className="accordion-header">
+							<button
+								className="accordion-button collapsed"
+								type="button"
+								data-bs-toggle="collapse"
+								data-bs-target="#panelsStayOpen-collapseFour"
+								aria-expanded="false"
+								aria-controls="panelsStayOpen-collapseFour"
+							>
+								Starting with heavy compliments
+							</button>
+						</h2>
+						<div
+							id="panelsStayOpen-collapseFour"
+							className="accordion-collapse collapse"
+						>
+							<div className="accordion-body">
+								Starting with heavy compliments is another sign
+								of AI-generated content [1].
+								<br></br>
+								<br></br>
+								Example: <br></br>
+								"That is a fantastic question! I'm glad you
+								asked it. The answer is..."
+							</div>
+						</div>
+					</div>
+					<div className="accordion-item">
+						<h2 className="accordion-header">
+							<button
+								className="accordion-button collapsed"
+								type="button"
+								data-bs-toggle="collapse"
+								data-bs-target="#panelsStayOpen-collapseFive"
+								aria-expanded="false"
+								aria-controls="panelsStayOpen-collapseFive"
+							>
+								Text lacking meaning
+							</button>
+						</h2>
+						<div
+							id="panelsStayOpen-collapseFive"
+							className="accordion-collapse collapse"
+						>
+							<div className="accordion-body">
+								Text lacking meaning is a common sign of
+								AI-generated content [1]. There are many ways
+								that this can manifest. Sometimes the text has
+								weird anologies and metaphors that don't make
+								sense [1]. Other times the text contains a lot
+								of filler words and expressions that don't add
+								any meaning to the text [1]. Quite often these
+								are also vague while remaining positive [1].
+								<br></br>
+								<br></br>
+								Example: <br></br>
+								"It's time to look forward. Today, I’m excited
+								to share a new beginning. I’m starting a company
+								focused on feeding students, with a simple
+								belief: when we elevate students’ lives with
+								practical solutions, we unlock outsized
+								potential. The current solutions are like
+								planting seeds on concrete" [1]
+							</div>
+						</div>
+					</div>
+					<div className="accordion-item">
+						<h2 className="accordion-header">
+							<button
+								className="accordion-button collapsed"
+								type="button"
+								data-bs-toggle="collapse"
+								data-bs-target="#panelsStayOpen-collapseSix"
+								aria-expanded="false"
+								aria-controls="panelsStayOpen-collapseSix"
+							>
+								Overuse of meta text and unnecessary
+								explanations
+							</button>
+						</h2>
+						<div
+							id="panelsStayOpen-collapseSix"
+							className="accordion-collapse collapse"
+						>
+							<div className="accordion-body">
+								Especially when answering questions, LLMs tend
+								to overuse meta text and unnecessary
+								explanations [1]. This can also be found with
+								unnecessary clarifications and summarisations
+								[1].
+							</div>
+						</div>
+					</div>
+					<div className="accordion-item">
+						<h2 className="accordion-header">
+							<button
+								className="accordion-button collapsed"
+								type="button"
+								data-bs-toggle="collapse"
+								data-bs-target="#panelsStayOpen-collapseSeven"
+								aria-expanded="false"
+								aria-controls="panelsStayOpen-collapseSeven"
+							>
+								Generic stories without using the first person
+							</button>
+						</h2>
+						<div
+							id="panelsStayOpen-collapseSeven"
+							className="accordion-collapse collapse"
+						>
+							<div className="accordion-body">
+								Humans tend to inject more of their personality
+								and experinces into their writing than LLMs do
+								[1]. Humans tend to use experssions suchs as "As
+								a...", "In my experience...", "I think..." and
+								"Personally..." more often than LLMs do [1].
+								<br></br>
+								<br></br>
+								Example: <br></br>
+								Human: "I congratulate you on your new job!"
+								<br></br>
+								AI: "Congratulations on your new job!"
+							</div>
+						</div>
+					</div>
+				</div>
+				<div>
+					<h3>Sources used:</h3>
+				</div>
+				<ol>
+					<li>
+						<a
+							href="https://www.youtube.com/watch?v=9Ch4a6ffPZY"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							I Can Spot AI Writing Instantly — Here’s How You Can
+							Too
+						</a>
+					</li>
+				</ol>
+				<div>
+					<h2>Image content coming later</h2>
+				</div>
 			</div>
 		</div>
 	);
