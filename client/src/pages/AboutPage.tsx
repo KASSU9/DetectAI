51e63354c3f3727bb4eb3100da0be30c810c9a54
Kasper Strang <kasper.strang@aalto.fi>
2026-04-02 21:39:32 +0300

    created simple about and guide pages

--- client/src/pages/AboutPage.tsx
+++ client/src/pages/AboutPage.tsx
@@ -1,5 +1,20 @@
 function AboutPage() {
-	return <h1>About</h1>;
+	return (
+		<div className="card">
+			<div className="card-body">
+				<h1>About DetectAI</h1>
+				<p>
+					DetectAI helps you to learn how to detect AI-generated
+					content. The game consists of a theory section and a quiz.
+					In theory section, you will learn about the different
+					techniques used to generate AI content and how to spot them.
+					In the quiz, you will be presented with different pieces of
+					content to practice your skills. The game is designed to be
+					fun and educational, and it is suitable for all ages.
+				</p>
+			</div>
+		</div>
+	);
 }
 
 export default AboutPage;
--- client/src/pages/GuidePage.tsx
+++ client/src/pages/GuidePage.tsx
@@ -1,5 +1,12 @@
 function GuidePage() {
-	return <h1>Guide</h1>;
+	return (
+		<div className="card">
+			<div className="card-body">
+				<h1>Guide</h1>
+				<p>This is a simple guide page for the DetectAI application.</p>
+			</div>
+		</div>
+	);
 }
 
 export default GuidePage;
